dc2c3a8153af46c2695e4a236934fdf9a3a991bf
Dennis <10233439+idea404@users.noreply.github.com>
2022-05-04 13:16:55 +0100

    first passing rs test

--- ava.config.cjs
+++ ava.config.cjs
@@ -1,13 +1,9 @@
-require('util').inspect.defaultOptions.depth = 5; // Increase AVA's printing depth
+require("util").inspect.defaultOptions.depth = 5; // Increase AVA's printing depth
 
 module.exports = {
-  timeout: '300000',
-  files: ['**/*.ava.ts'],
+  timeout: "300000",
+  files: ["**/*.ava.ts"],
   failWithoutAssertions: false,
-  extensions: [
-    'ts'
-  ],
-  require: [
-    'ts-node/register',
-  ],
+  extensions: ["ts"],
+  require: ["ts-node/register"],
 };
